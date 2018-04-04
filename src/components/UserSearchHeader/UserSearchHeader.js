57e3a52151ae5976adad1a840ccaafc5aa4b8866
shahzebjaved23 <shahzebjaved23@gmail.com>
2018-04-05 03:16:17 +0500

    adds the add new item

--- src/components/UserSearchHeader/UserSearchHeader.js
+++ src/components/UserSearchHeader/UserSearchHeader.js
@@ -5,6 +5,12 @@ class UserSearchHeader extends Component{
 
 	constructor(props){
 		super(props)
+		this.state = {
+			namePlaceholder: "Search Name",
+			emailPlaceHolder: "Search Email",
+			phonePlaceHolder: "Search Phone Number",
+			saveButtonText: "Add New"
+		}
 	}
 
 	handleSubmit(e){
@@ -35,13 +41,58 @@ class UserSearchHeader extends Component{
 			}
 		})
 
-
-
 		localStorage.setItem("usersList", JSON.stringify(newUsersList));
 
-		setTimeout(()=>{
-			this.props.resetUsersList()
-		},1000)
+		this.props.resetUsersList()
+	}
+
+	setAddState(){
+		this.setState({
+			namePlaceholder: "Add Name",
+			emailPlaceHolder: "Add Email",
+			phonePlaceHolder: "Add Phone Number",
+			saveButtonText: "Save"
+		})
+	}
+
+	setSearchState(){
+		this.setState({
+			namePlaceholder: "Search Name",
+			emailPlaceHolder: "Search Email",
+			phonePlaceHolder: "Search Phone Number",
+			saveButtonText: "Add New"
+		})
+	}
+
+	addUser(){
+		let usersList = JSON.parse(localStorage.getItem("usersList"));
+		let newUserId = usersList[usersList.length-1].id + 1;
+		usersList.push({
+			id: newUserId, 
+			name: this.refs.name.value, 
+			email: this.refs.email.value, 
+			phone_number: this.refs.phone_number.value
+		})
+		localStorage.setItem("usersList", JSON.stringify(usersList));
+		this.props.resetUsersList()	
+		this.setSearchState()
+		this.resetRefValues()
+	}
+
+	resetRefValues(){
+		this.refs.name.value = "";
+		this.refs.email.value = "";
+		this.refs.phone_number.value = "";
+	}
+
+	handleAddNew(){
+		if(this.state.saveButtonText == "Add New"){
+			this.setAddState()
+		}
+
+		if(this.state.saveButtonText == "Save"){
+			this.addUser()
+		}
 		
 	}
 
@@ -52,16 +103,18 @@ class UserSearchHeader extends Component{
 					<div className="row">
 						<div className="col-md-1"></div>
 						<div className="col-md-2">
-							<input ref="name" placeholder="Search Name" />
+							<input ref="name" placeholder={this.state.namePlaceholder} />
 						</div>
 						<div className="col-md-2">
-							<input ref="email" placeholder="Search Email" />
+							<input ref="email" placeholder={this.state.emailPlaceHolder} />
 						</div>
 						<div className="col-md-2">
-							<input ref="phone_number" placeholder="Search Phone Number" />
+							<input ref="phone_number" placeholder={this.state.phonePlaceHolder} />
 						</div>
 						<div className="col-md-2"></div>
-						<div className="col-md-2"></div>
+						<div className="col-md-2">
+							<button type="button" onClick={this.handleAddNew.bind(this)}>{this.state.saveButtonText}</button>
+						</div>
 					</div>
 					<input type="submit" style={{ display: "none" }} />
 				</form>
