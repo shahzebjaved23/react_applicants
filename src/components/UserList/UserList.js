e9fce56ed56b5c9175894d74c0ecac8b54b908cd
shahzebjaved23 <shahzebjaved23@gmail.com>
2018-04-08 14:18:59 +0500

    fixes for the sort

--- src/components/UserList/UserList.js
+++ src/components/UserList/UserList.js
@@ -8,25 +8,48 @@ class UserList extends Component {
 	constructor(props){
 		super(props);
 		this.state = {
-			usersList: JSON.parse(localStorage.getItem("usersList"))
+			usersList: JSON.parse(localStorage.getItem("usersList")),
+			listSortAsc: true 
 		}
+	}
+
+	dynamicSort(property) {
+	    var sortOrder = 1;
+    	if(this.state.listSortAsc){
+    		sortOrder = -1;
+    		this.setState({ listSortAsc: false })
+    	}else{
+    		sortOrder = 1;	
+    		this.setState({ listSortAsc: true })
+    	}
+        
+        return function (a,b) {
+	        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+	        return result * sortOrder;
+	    }
+	}
+
+	sortById(){
+		this.setState({
+			usersList: this.state.usersList.sort(this.dynamicSort("id"))
+		})
 	}
 
 	sortByName(){
 		this.setState({
-			usersList: this.state.usersList.sort((a, b) => a.name > b.name)
+			usersList: this.state.usersList.sort(this.dynamicSort("name"))
 		})
 	}
 
 	sortByEmail(){
 		this.setState({
-			usersList: this.state.usersList.sort((a, b) => a.email > b.email)
+			usersList: this.state.usersList.sort(this.dynamicSort("email"))
 		})
 	}
 
 	sortByPhoneNumber(){
 		this.setState({
-			usersList: this.state.usersList.sort((a, b) => a.phone_number > b.phone_number)
+			usersList: this.state.usersList.sort(this.dynamicSort("phone_number"))
 		})
 	}
 
@@ -56,7 +79,9 @@ class UserList extends Component {
 
 					<div className="user-header-row">	
 						<div className="row user-header-row-div">
-							<div className="col-md-1">Id</div>
+							<div className="col-md-1">
+								<a onClick={this.sortById.bind(this)}>Id</a>
+							</div>
 							<div className="col-md-2">
 								<a onClick={this.sortByName.bind(this)}>Name</a>
 							</div>
